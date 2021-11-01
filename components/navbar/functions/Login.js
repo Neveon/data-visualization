c063a7c497684d2e04d9124e799aebe0e9140c0a
Neveon <41444780+Neveon@users.noreply.github.com>
2021-11-01 03:14:17 -0400

    Turned login component into modal

--- components/navbar/functions/Login.js
+++ components/navbar/functions/Login.js
@@ -1,20 +1,75 @@
 // Login form
 import { DataContext } from "../../../App";
+import $ from "../../../node_modules/jquery";
 
 const Login = () => {
-    const onSubmit = (e) => {
+    const [user, setUser] = React.useState("");
+    const [pass, setPass] = React.useState("");
+
+    const handleSubmit = (e) => {
         e.preventDefault();
     };
+
     return (
-        <form action="login.php" method="post">
-            <label htmlFor="username">Username:</label>
-            <input type="text" name="username" id="username" />
-
-            <label htmlFor="password"></label>
-            <input type="password" name="password" id="username" />
-
-            <input type="submit" value="Submit" />
-        </form>
+        <div
+            className="modal fade"
+            id="login"
+            tabIndex="-1"
+            role="dialog"
+            aria-labelledby="login-label"
+            aria-hidden="true"
+        >
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="login-label">
+                            Login Form
+                        </h5>
+                        <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="username">Username: </label>
+                            <input
+                                type="text"
+                                name="username"
+                                id="username"
+                                value={user}
+                                onChange={(e) => setUser(e.target.value)}
+                            />
+                            <br />
+                            <label htmlFor="password">Password: </label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                value={pass}
+                                onChange={(e) => setPass(e.target.value)}
+                            />
+                            <br />
+                            <br />
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
